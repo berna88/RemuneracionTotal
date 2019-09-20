69728a1e583450b0e865784224be163d9fee504c
Bernardo <berna_halo@hotmail.com>
2019-09-20 16:56:33 -0500

    Initial commit

---
+++ src/main/java/com/consistent/cuervo/constants/ServiceRemuneracionPortletKeys.java
@@ -0,0 +1,10 @@
+package com.consistent.cuervo.constants;
+
+/**
+ * @author bernardohernandez
+ */
+public class ServiceRemuneracionPortletKeys {
+
+	public static final String ServiceRemuneracion = "serviceremuneracion";
+
+}
